667339a5e6961699bd41106cca495811aed6d3a4
Jonathan Whetsell <7316327+jrwhetse@users.noreply.github.com>
2025-05-27 16:18:21 +0000

    GH-38: Add and document Maven build/test troubleshooting and resolution history

---
+++ sb/src/test/java/com/vha/sb/SbApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vha.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
